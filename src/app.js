90d4227a82c7439c9697c7bbf66d49b0a03e0a6a
Regina7411 <danielagg71411@gmail.com>
2025-03-10 22:51:24 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-import "bootstrap";
-import "./style.css";
+function generateExcuse() {
+  const subjects = ["A lonely merchant", "My crazy neighbor", "A stray cat", "A wizard", "A time traveler"];
+  const actions = ["kicked", "stole", "burned", "teleported", "transformed"];
+  const objects = ["my car", "my homework", "my shoes", "my phone", "my sandwich"];
+  const locations = ["near the toilet", "in the park", "at my house", "on the moon", "under the bed"];
+  
+  let excuse = `${subjects[Math.floor(Math.random() * subjects.length)]} ` +
+               `${actions[Math.floor(Math.random() * actions.length)]} ` +
+               `${objects[Math.floor(Math.random() * objects.length)]} ` +
+               `${locations[Math.floor(Math.random() * locations.length)]}`;
+  
 
+  document.getElementById("excuse").innerText = excuse;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = generateExcuse;
